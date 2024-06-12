52b1e4b4f1df07842c440567cdcb0fa4a6cd97f1
batufidan <batuhan.fidan@ogr.iu.edu.tr>
2024-06-12 16:08:26 +0300

    [PREPARE] aceternity ui

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,9 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <h1>Batuhan Fidan</h1>;
+  return (
+    <h1 className="w-full md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden ">
+      Batuhan Fidan
+    </h1>
+  );
 }
