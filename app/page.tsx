859126ec36d9c5240cbc276469a93f731ed23f1b
batufidan <batuhan.fidan@ogr.iu.edu.tr>
2024-06-29 15:00:57 +0300

    [ADD] pricing section

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Navbar from "@/components/Navbar";
 import SliderOne from "@/components/ui/Slider";
 import { Spotlight } from "@/components/ui/Spotlight";
@@ -9,11 +10,41 @@ import ShopifyStores from "./shopify-stores";
 import Brands from "./brands";
 import Services from "./services";
 import Faq from "./faq";
+import { useRef, useState } from "react";
+import Pricing from "./pricing/page";
 
 export default function Home() {
+  const websiteDesignRef = useRef<HTMLDivElement>(null);
+  const graphicDesignRef = useRef<HTMLDivElement>(null);
+  const shopifyStoresRef = useRef<HTMLDivElement>(null);
+  const brandsRef = useRef<HTMLDivElement>(null);
+
+  const scrollToWebsiteDesign = () => {
+    websiteDesignRef.current?.scrollIntoView({
+      behavior: "smooth",
+    });
+  };
+
+  const scrollToGraphicDesign = () => {
+    graphicDesignRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToShopifyStores = () => {
+    shopifyStoresRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  const scrollToBrands = () => {
+    brandsRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
   return (
     <h1 className="w-full  md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden ">
-      <Navbar />
+      <Navbar
+        scrollToWebsiteDesign={scrollToWebsiteDesign}
+        scrollToGraphicDesign={scrollToGraphicDesign}
+        scrollToShopifyStores={scrollToShopifyStores}
+        scrollToBrands={scrollToBrands}
+      />
       <Spotlight className="hidden md:flex left-80 " fill="white" />
       <div className="p-4 mx-auto relative z-10 w-full pt-10 md:pt-20 px-2">
         <div className="text-4xl pb-5 md:text-7xl px-6 text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 bg-neutral-400 bg-opacity-50">
@@ -32,13 +63,24 @@ export default function Home() {
 
         <div className="w-full pt-20">
           <SliderOne />
-          <WebsiteDesign />
-          <GraphicDesign />
-          <ShopifyStores />
-          <Brands />
-          <Services />
-          <Faq />
         </div>
+        <div ref={websiteDesignRef}>
+          <WebsiteDesign />
+        </div>
+        <div ref={graphicDesignRef}>
+          <GraphicDesign />
+        </div>
+        <div ref={shopifyStoresRef}>
+          <ShopifyStores />
+        </div>
+        <div ref={brandsRef}>
+          <Brands />
+        </div>
+        <div id="services">
+          <Services />
+        </div>
+        <Faq />
+        <Pricing />
       </div>
     </h1>
   );
